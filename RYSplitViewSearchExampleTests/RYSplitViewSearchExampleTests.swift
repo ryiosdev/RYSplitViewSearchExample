b8f24680e8a0bbcba698dc4f35042c3d0e945688
RYiOSDev <1859295+ryiosdev@users.noreply.github.com>
2025-02-15 22:11:22 -0600

    Initial Commit

---
+++ RYSplitViewSearchExampleTests/RYSplitViewSearchExampleTests.swift
@@ -0,0 +1,16 @@
+//
+//  RYSplitViewSearchExampleTests.swift
+//  RYSplitViewSearchExampleTests
+//
+//  Created by Ryan Young on 2/15/25.
+//
+
+import Testing
+
+struct RYSplitViewSearchExampleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
